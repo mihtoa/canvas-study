e1d1bc9cedc461b9672eff0061aa74f38d352f9c
Milene Toazza <oi@mihtoa.ppg.br>
2018-08-01 20:26:30 -0300

    Create PacMan canvas

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+let stage = document.createElement('canvas');
+stage.width = 300;
+stage.height = 300;
+stage.style.background = 'rgba(0,0,0,.8)'
+document.body.appendChild(stage);
+
+let cnv = stage.getContext('2d');
+let w = stage.width;
+let h = stage.height;
+let r = 30;
+let angle = 1;
+let limitOpen = 40;
+let colorPacMan = 'yellow';
+let opening = true;
+let interval = 20;
+
+function draw() {
+  cnv.fillStyle = 'black';
+  cnv.strokeStyle = 'black';
+  cnv.lineWidth = 20;
+  cnv.arc(w / 2, h / 2, r, 0, Math.PI * 2);
+  cnv.stroke();
+  cnv.fill();
+  ang = (Math.PI/180)*angle;
+
+  cnv.beginPath();
+  cnv.fillStyle = colorPacMan;
+  cnv.strokeStyle = colorPacMan;
+  cnv.lineWidth = 40;
+  cnv. arc(w/2, h/2, r-10, ang, -ang);
+  cnv.stroke();
+}
+
+function animate() {
+  setInterval(function(){
+    draw();
+    if(opening) {
+      ++angle;
+    } else {
+      --angle
+    }
+    if(angle <= 1 || angle >= limitOpen ) {
+      opening = angle <= 1 ? true : false;
+    }
+  }, interval);
+}
+
+animate();
+
